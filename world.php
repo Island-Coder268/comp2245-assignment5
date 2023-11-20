dd3ee301b4e10769509b3ed4f3ed92ea946c85e1
Caleb Gardiner <joshua.gardiner268@gmail.com>
2023-11-20 03:38:18 -0400

    Completed Exercise 1-4

--- world.php
+++ world.php
@@ -9,52 +9,48 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 // Get the country from the query string
 $country = isset($_GET['country']) ? $_GET['country'] : '';
 
-// Get the city from the query string
-$lookup = isset($_GET['city']) ? $_GET['city'] : '';
-
-if ($lookup == 'cities') {
-    $select = $conn->prepare("SELECT cities.name, cities.district, cities.population
-                             FROM countries
-                             INNER JOIN cities ON countries.code = cities.country_code
-                             WHERE countries.name LIKE :country");
-    $select->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
-    $select->execute();
-    $results = $select->fetchAll(PDO::FETCH_ASSOC);
-} else {
-    $select = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
-    $select->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
-    $select->execute();
-    $results = $select->fetchAll(PDO::FETCH_ASSOC);
-}
-
+$select = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+$select->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
+$select->execute();
+$results = $select->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<?php if ($lookup == 'cities'): ?>
-    <!-- Render the table for cities -->
-    <table>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Country Lookup Results</title>
+    <!-- Additional head elements as needed -->
+</head>
+
+<body>
+
+<?php if (!empty($results)): ?>
+    <table border="1">
         <thead>
             <tr>
-                <th>Name</th>
-                <th>Population</th>
-                <th>District</th>
+                <th>Country Name</th>
+                <th>Continent</th>
+                <th>Independence Year</th>
+                <th>Head of State</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($results as $row): ?>
                 <tr>
                     <td><?= $row['name']; ?></td>
-                    <td><?= $row['population']; ?></td>
-                    <td><?= $row['district']; ?></td>
+                    <td><?= $row['continent']; ?></td>
+                    <td><?= $row['independence_year']; ?></td>
+                    <td><?= $row['head_of_state']; ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
 <?php else: ?>
-    <!-- Render the list for countries -->
-    <ul>
-        <?php foreach ($results as $row): ?>
-            <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <p>No results found for the specified country.</p>
 <?php endif; ?>
 
+</body>
+
+</html>
