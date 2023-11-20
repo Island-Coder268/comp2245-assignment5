272e7bad6705d44610283fb023357f365061c80b
Caleb Gardiner <joshua.gardiner268@gmail.com>
2023-11-20 04:25:05 -0400

    Completed Exercises 1-5

--- world.js
+++ world.js
@@ -1,5 +1,3 @@
-// world.js
-
 document.addEventListener("DOMContentLoaded", function() {
     // Listen for clicks on the "Lookup" button
     var lookupButton = document.getElementById("lookup");
@@ -9,18 +7,36 @@ document.addEventListener("DOMContentLoaded", function() {
         var countryValue = countryInput.value;
 
         // Make an AJAX request to world.php with the entered country value
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "world.php?country=" + encodeURIComponent(countryValue), true);
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                // Handle the response and update the "result" div
-                var resultDiv = document.getElementById("result");
-                resultDiv.innerHTML = xhr.responseText;
-            }
-        };
-
-        // Send the AJAX request
-        xhr.send();
+        makeAjaxRequest("world.php?country=" + encodeURIComponent(countryValue));
     });
+
+    // Add a new event listener for the "LookupCities" button 
+    var lookupCitiesButton = document.getElementById("lookupCities");
+    if (lookupCitiesButton) {
+        lookupCitiesButton.addEventListener("click", function() {
+            // Get the value entered by the user in the country input
+            var countryInput = document.getElementById("country");
+            var countryValue = countryInput.value;
+
+            // Make an AJAX request to world.php with the entered country value and lookup=cities
+            makeAjaxRequest("world.php?country=" + encodeURIComponent(countryValue) + "&lookup=cities");
+        });
+    }
 });
+
+function makeAjaxRequest(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // Handle the response and update the "result" div
+            var resultDiv = document.getElementById("result");
+            resultDiv.innerHTML = xhr.responseText;
+        }
+    };
+
+    // Send the AJAX request
+    xhr.send();
+}
+
--- world.php
+++ world.php
@@ -9,10 +9,27 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 // Get the country from the query string
 $country = isset($_GET['country']) ? $_GET['country'] : '';
 
-$select = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
-$select->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
-$select->execute();
-$results = $select->fetchAll(PDO::FETCH_ASSOC);
+// Check for the lookup query parameter
+$lookup = isset($_GET['lookup']) ? $_GET['lookup'] : '';
+
+if ($lookup == 'cities') {
+    // SQL query for cities lookup
+    $select = $conn->prepare("SELECT cities.name AS city_name, cities.district, cities.population
+                             FROM countries
+                             INNER JOIN cities ON countries.code = cities.country_code
+                             WHERE countries.name LIKE :country");
+    $select->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
+    $select->execute();
+    $results = $select->fetchAll(PDO::FETCH_ASSOC);
+} else {
+    // Default SQL query for country lookup
+    $select = $conn->prepare("SELECT countries.name AS country_name, countries.continent, countries.independence_year, countries.head_of_state
+                             FROM countries
+                             WHERE countries.name LIKE :country");
+    $select->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
+    $select->execute();
+    $results = $select->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,33 +37,56 @@ $results = $select->fetchAll(PDO::FETCH_ASSOC);
 
 <head>
     <meta charset="utf-8">
-    <title>Country Lookup Results</title>
+    <title>World Database Lookup</title>
     <!-- Additional head elements as needed -->
 </head>
 
 <body>
 
 <?php if (!empty($results)): ?>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Country Name</th>
-                <th>Continent</th>
-                <th>Independence Year</th>
-                <th>Head of State</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($results as $row): ?>
+    <?php if ($lookup == 'cities'): ?>
+        <!-- Output table for cities -->
+        <table border="1">
+            <thead>
                 <tr>
-                    <td><?= $row['name']; ?></td>
-                    <td><?= $row['continent']; ?></td>
-                    <td><?= $row['independence_year']; ?></td>
-                    <td><?= $row['head_of_state']; ?></td>
+                    <th>City Name</th>
+                    <th>District</th>
+                    <th>Population</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($results as $row): ?>
+                    <tr>
+                        <td><?= $row['city_name']; ?></td>
+                        <td><?= $row['district']; ?></td>
+                        <td><?= $row['population']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <!-- Output table for countries -->
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Country Name</th>
+                    <th>Continent</th>
+                    <th>Independence Year</th>
+                    <th>Head of State</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($results as $row): ?>
+                    <tr>
+                        <td><?= $row['country_name']; ?></td>
+                        <td><?= $row['continent']; ?></td>
+                        <td><?= $row['independence_year']; ?></td>
+                        <td><?= $row['head_of_state']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
 <?php else: ?>
     <p>No results found for the specified country.</p>
 <?php endif; ?>
